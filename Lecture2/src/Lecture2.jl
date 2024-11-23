465638d55744c1fd8f42868bfc30f120657e4931
catherine4331 <catherine4331@gmail.com>
2024-11-23 19:42:25 +0000

    Lots of lecture 2 stuff

---
+++ Lecture2/src/Lecture2.jl
@@ -0,0 +1,82 @@
+A = 1:9
+
+sz = (3, 3)
+
+i = 3
+j = 2
+
+A[i + (j-1)*3]
+
+A = rand(100,100)
+B = rand(100,100)
+C = rand(100,100)
+using BenchmarkTools
+function inner_rows!(C,A,B)
+  for i in 1:100, j in 1:100
+    C[i,j] = A[i,j] + B[i,j]
+  end
+end
+@btime inner_rows!(C,A,B)
+
+function inner_cols!(C,A,B)
+    for j in 1:100, i in 1:100
+      C[i,j] = A[i,j] + B[i,j]
+    end
+  end
+@btime inner_cols!(C,A,B)
+
+function inner_alloc!(C,A,B)
+    for j in 1:100, i in 1:100
+      val = [A[i,j] + B[i,j]]
+      C[i,j] = val[1]
+    end
+  end
+@btime inner_alloc!(C,A,B)
+
+function inner_alloc(A,B)
+    C = similar(A)
+    for j in 1:100, i in 1:100
+      val = A[i,j] + B[i,j]
+      C[i,j] = val[1]
+    end
+
+    C
+  end
+@btime inner_alloc(A,B)
+
+function inner_noalloc!(C,A,B)
+    for j in 1:100, i in 1:100
+      val = A[i,j] + B[i,j]
+      C[i,j] = val[1]
+    end
+  end
+@btime inner_noalloc!(C,A,B)
+
+using StaticArrays
+val = SVector{3, Float64}(1.0, 2.0, 3.0)
+typeof(val)
+
+function static_inner_alloc!(C,A,B)
+  for j in 1:100, i in 1:100
+    val = @SVector [A[i,j] + B[i,j]]
+    C[i,j] = val[1]
+  end
+end
+@btime static_inner_alloc!(C,A,B)
+
+@macroexpand @SVector [A[i,j] + B[i,j]]
+
+function f(A, B)
+    # 10 * (A + B)
+    C = similar(A)
+    for k in 1:10
+        # A + B
+        C .+= A .+ B
+    end
+
+    C
+end
+@btime f(A, B)
+
+fused(A, B, C) = A .+ B .+ C
+@btime fused(A, B, C)
